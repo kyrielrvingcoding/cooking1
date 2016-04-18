3ad2f7e80d586e8277b247362f99ef358a7b790c
kyrielrvingcoding <kyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.comkyrie.lrving.coding@gmail.com123456>
2016-04-18 21:37:54 +0800

    奶嘴第一次上传

---
+++ cooking1/cooking1/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  cooking1
+//
+//  Created by 诸超杰 on 16/4/18.
+//  Copyright © 2016年 诸超杰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"奶嘴第一次上传");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
